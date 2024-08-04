537aae8f1b777f5c550a9f61275bf35a6da56edd
frenchfries-9320 <87230106+frenchfries-9320@users.noreply.github.com>
2024-08-04 20:51:23 +0900

    understand const final var

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ui_update_demo/keys/keys.dart';
 
 void main() {
-  final numbers = {1, 2, 3};
+  var numbers = {1, 2, 3};
+  numbers = {4, 5, 6};
   numbers.add(4);
   runApp(const App());
 }
